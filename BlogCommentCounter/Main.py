4e616d0528d109dc82eba18f6cfa244fbc798a74
Josef John <josefjohn88@gmail.com>
2013-03-19 02:19:39 -0800

    basic functionality, main problem is it only pulls one comment from each blog post at the moment

--- BlogCommentCounter/Main.py
+++ BlogCommentCounter/Main.py
@@ -33,5 +33,31 @@ def get_all_blog_posts(url):
                     if str(blogpost) not in posts:
                         posts.add(str(blogpost))
     return posts
-                                
-print get_all_blog_posts(my_url)
+
+#partially working, only returns 1 comment from each post even if there are more than 1!!!!!!!!!!!!
+#takes a set of urls that link to blog posts
+#returns a list of JSON objects, each object is a dictionary containing a comment
+def get_all_comments(set_of_links):
+    comments = []
+    for link in set_of_links:
+        #print link
+        html = urllib2.urlopen(link)
+        for line in html:
+            if ('?showComment' in line):
+                begin = line.find('var items = [{') + len('var items = [')
+                end = line.find('}];') + 1
+                item = line[begin:end]
+                comments.append(item)
+    return comments
+
+list_of_comments = get_all_comments(get_all_blog_posts(my_url))
+
+#just testing to see if it works, change later
+for item in list_of_comments:
+    if ('name' in item and 'body' in item):
+        body_start =item.find('body\': ') + len('body\': \'')
+        body_end = item[body_start:].find('\'')
+        body = item[body_start:body_start+body_end]
+        name_start = item.find('{\'name\': ') + len('{\'name\': \'')
+        name_end = item[name_start:].find('\'')
+        print item[name_start:name_start+name_end] +':', item[body_start:body_start+body_end]
