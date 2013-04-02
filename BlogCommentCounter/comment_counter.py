06bfc55d905491e38b1eb2dc2d04276a2bdd5020
Gautam Jain <gautamjain@gmail.com>
2013-04-01 18:14:29 -0700

    Linked code.

--- BlogCommentCounter/comment_counter.py
+++ BlogCommentCounter/comment_counter.py
@@ -1,18 +1,11 @@
 # Authors: Gautam Jain & Josef John
 
+import math
 import pprint
 import gspread
 import rfc3339
 from apiclient.discovery import build
 from urlparse import urlparse
-
-#######################################################
-## TO BE EDITED
-start_of_week = [1359417600, 1360022400, 1360627200, 1361232000, 1361836800,
-                 1362441600, 1363046400, 1363651200, 1364256000, 1364860800,
-                 1365465600, 1366070400, 1366675200, 1367280000, 1367884800,
-                 1368489600, 1369174400]
-#######################################################
 
 # Google Docs Worksheet Info
 wks_name = 'Test Spreadsheet'
@@ -23,6 +16,8 @@ sem_end = '2013-05-05'    # Must be a Sunday
 
 sem_start = rfc3339.strtotimestamp(sem_start + 'T00:00:00-08:00')
 sem_end = rfc3339.strtotimestamp(sem_end + 'T23:59:59-07:00')
+
+week_in_seconds = 604800
 
 # Convert times to RFC3339 format as required by Blogger API
 sem_start_rfc3339 = rfc3339.timestamptostr(sem_start)
@@ -82,17 +77,36 @@ def format_url(url):
 # Returns a list of post IDs for the given blog that were
 # published between sem_start and sem_end
 def get_posts(blog_id):
-    # Request a list of posts between specified dates. Only return post IDs.
-    response = blogger.posts().list(
-        blogId=blog_id,
-        startDate=sem_start_rfc3339,
-        endDate=sem_end_rfc3339,
-        fields='items/id').execute()
-
-    # Reformat response into a list
     post_ids = []
-    for post in response.get('items', []):
-        post_ids.append(str(post['id']))
+    page_token = ''
+    while True:
+        # Request a list of posts between specified dates. Only return post IDs.
+        if (page_token == ''):
+            response = blogger.posts().list(
+                blogId=blog_id,
+                startDate=sem_start_rfc3339,
+                endDate=sem_end_rfc3339,
+                maxResults=10,
+                fields='items/id,nextPageToken').execute()
+        else:
+            response = blogger.posts().list(
+                blogId=blog_id,
+                startDate=sem_start_rfc3339,
+                endDate=sem_end_rfc3339,
+                pageToken=page_token,
+                maxResults=10,
+                fields='items/id,nextPageToken').execute()
+
+        # Reformat response into a list
+        for post in response.get('items', []):
+            post_ids.append(str(post['id']))
+
+        page_token = str(response.get('nextPageToken'))
+
+        # Check if there are no more 'pages'
+        if page_token == 'None':
+            break
+
     return post_ids
 
 
@@ -136,6 +150,15 @@ def get_all_comments(blog_id):
     return comments
 
 
+def get_author(blog_id, post_id):
+    response = blogger.posts().get(
+        blogId=blog_id,
+        postId=post_id,
+        fields='author/id').execute()
+
+    return str(response.get('author')['id'])
+
+
 #returns list of dictionaries
 #each dictionary maps to a week
 #each dictionary contains author_id keys mapping to a set of comment_ids for that week
@@ -151,6 +174,9 @@ def get_total_comments(blog_ids):
     for blog_id in blog_ids:
         post_ids = get_posts(blog_id)
 
+        # Request and save author ID for later use
+        blog_authors_dict[get_author(blog_id, post_ids[0])] = blog_id
+
         # Iterate through every post
         for post_id in post_ids:
             list_of_comments_dict = get_comments(blog_id, post_id)
@@ -160,12 +186,9 @@ def get_total_comments(blog_ids):
                 author_id = comment['author_id']
                 comment_id = comment['comment_id']
                 publish_date = comment['publish_date']
+                week = get_week(publish_date)
+                print str(week),
 
-                week = get_week_from_publish_date(publish_date)
-                if week < 0 or week > 16:
-                    print "error"
-                    # Change this
-                    return
                 week_comments_dict = total_comments_dictionary_for_week[week]
 
                 if author_id not in week_comments_dict.keys():
@@ -173,19 +196,14 @@ def get_total_comments(blog_ids):
                 
                 week_comments_dict[author_id].append(comment_id)
 
-                # if author_id in week_comments_dict.keys():
-                #     pprint.pprint(week_comments_dict[author_id])
-                #     week_comments_dict[author_id] = week_comments_dict[author_id].add(comment_id)
-                # else:
-                #     # set with comment_ids
-                #     set_of_comment_ids = set()
-                #     set_of_comment_ids.add(comment_id)
-                #     week_comments_dict[author_id] = set_of_comment_ids
-                #     pprint.pprint(week_comments_dict)
-
     return total_comments_dictionary_for_week
 
-          
+
+def get_week(publish_date_rfc3339):
+    publish_date = rfc3339.strtotimestamp(publish_date_rfc3339)
+    return int(math.floor((publish_date - sem_start) / week_in_seconds))
+
+
 def get_week_from_publish_date(publish_date):
     date_in_long = rfc3339.strtotimestamp(publish_date)
     for i in range(0, len(start_of_week) - 1):
@@ -193,6 +211,7 @@ def get_week_from_publish_date(publish_date):
             #print "week ", i
             return i
     return -1
+
 
 #####################################################################
 #for each url, get all comments
@@ -236,9 +255,30 @@ for blog_URL in blog_URLs:
     blog_ID = get_blog_id(blog_URL)
     blog_IDs_dict[blog_URL] = blog_ID
 
+print '######### BLOG_URL -> BLOG_ID'
 pprint.pprint(blog_IDs_dict)
 
-print '################################'
-print '################################'
-print '################################'
+print '######### PROCESSED DATA'
 pprint.pprint(get_total_comments(blog_IDs_dict.itervalues()))
+
+print '######### AUTHOR_ID -> BLOG_ID'
+pprint.pprint(blog_authors_dict)
+
+
+# Write to Google Docs Spreadsheet
+
+# Connect to Google Docs spreadsheet and get column with header 'Blog_URL'
+wks = gc.open(wks_name).sheet1
+
+url_header_col = wks.find('Blog_URL').col
+
+blog_URLs_list = wks.col_values(url_header_cell.col)
+
+# Filter through URLs to remove any non-Blogger entries
+blog_URLs_list = filter(is_blogger_URL, blog_URLs_list)
+
+week_header_cell = wks.find('Week_1')
+blog_URLs_list = wks.col_values(week_header_cell.col)
+
+# Filter through URLs to remove any non-Blogger entries
+blog_URLs_list = filter(is_blogger_URL, blog_URLs_list)
